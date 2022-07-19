ce3c9b5fd4656adfa9aefd857be8d67a8eb867d6
DhanushM12 <dhanushmukhi@gmail.com>
2022-07-19 21:25:50 +0530

    added code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import '../App.css';
+import React, {useState} from 'react';
 
 function App() {
+  let time =  new Date().toLocaleTimeString();
+  const [currentTime, setCurrentTime] = useState(time);
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  }
+
+  setInterval(updateTime, 1000)
   return (
-    <div className="App">
-      Hello 
+    <div className='App'>
+      <h1>{currentTime}</h1>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
